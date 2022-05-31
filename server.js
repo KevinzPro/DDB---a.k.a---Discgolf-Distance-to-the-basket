115baba9fb3712eed4337d5ae3ecdc9c4a2d2cbc
MattiasPro <mattiasm@proekspert.ee>
2022-05-31 11:06:53 +0300

    server setupp

--- server.js
+++ server.js
@@ -3,13 +3,88 @@ const mongo = require("mongodb").MongoClient
 
 const app = express()
 
+app.post("/trip", (req, res) => {
+  const name = req.body.name
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    console.log(result)
+    res.status(200).json({ ok: true })
+  })
+})
 
-app.post("/parks", (req, res) => {
-    /* */
+
+app.get("/trips", (req, res) => {
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    res.status(200).json({ trips: items })
   })
-app.get("/parks", (req, res) => {
-    /* */
+})
+
+
+app.post("/expense", (req, res) => {
+  expenses.insertOne(
+  {
+    trip: req.body.trip,
+    date: req.body.date,
+    amount: req.body.amount,
+    category: req.body.category,
+    description: req.body.description,
+  },
+  (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    res.status(200).json({ ok: true })
+  }
+  )
   })
+  app.get("/expenses", (req, res) => {
+    expenses.find({ trip: req.body.trip }).toArray((err, items) => {
+      if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+      }
+      res.status(200).json({ expenses: items })
+    })
+  })
+
+
+
+
 
 
 app.listen(3000, () => console.log("Server ready"))
+
+const url = "mongodb://localhost:27017"
+
+let db, trips, expenses
+
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db("tripcost")
+    trips = db.collection("trips")
+    expenses = db.collection("expenses")
+  }
+)
+
+app.use(express.json())
