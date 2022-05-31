fdb35c337de70fd5308ee778032922c57def51cf
MattiasPro <mattiasm@proekspert.ee>
2022-05-31 10:43:35 +0300

    server created

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const mongo = require("mongodb").MongoClient
+
+const app = express()
+
+
+app.post("/parks", (req, res) => {
+    /* */
+  })
+app.get("/parks", (req, res) => {
+    /* */
+  })
+
+
+app.listen(3000, () => console.log("Server ready"))
